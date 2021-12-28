cf92007d96bb3a97876621e97c1dc8c44b0f5e5d
vsantana <vsantana@sjcc.com.br>
2021-12-28 18:40:04 -0300

    :recycle: Migrating our states and effects to React Hooks.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import style from "../styles/Home.module.css";
 import { fetchPosts } from "../utils/fecthPosts";
 import Posts from "../components/Posts";
@@ -6,87 +6,80 @@ import Head from "next/head";
 import Button from "../components/Button";
 import Input from "../components/Input";
 
-class Home extends React.Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerpage: 3,
-    searchValue: "",
-  };
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerpage] = useState(3);
+  const [searchValue, setSearchValue] = useState("");
 
-  URL_POSTS = `https://jsonplaceholder.typicode.com/posts`;
-  URL_PHOTOS = `https://jsonplaceholder.typicode.com/photos`;
+  const URL_POSTS = `https://jsonplaceholder.typicode.com/posts`;
+  const URL_PHOTOS = `https://jsonplaceholder.typicode.com/photos`;
+  const noMorePosts = page + postsPerpage >= allPosts.length;
 
-  handleFetchPosts = async () => {
-    const { page, postsPerpage } = this.state;
-    const postsAndPhotos = await fetchPosts(this.URL_POSTS, this.URL_PHOTOS);
+  const filteredPosts = !!searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : posts;
 
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerpage),
-      allPosts: postsAndPhotos,
-    });
-  };
+  const handleFetchPosts = useCallback(async () => {
+    const postsAndPhotos = await fetchPosts(URL_POSTS, URL_PHOTOS);
+    setPosts(postsAndPhotos.slice(0, postsPerpage));
+    setAllPosts(postsAndPhotos);
 
-  loadMorePosts = () => {
-    const { page, postsPerpage, allPosts, posts } = this.state;
+  }, [URL_POSTS, URL_PHOTOS, postsPerpage]);
+
+  const loadMorePosts = () => {
     const nextPage = page + postsPerpage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerpage);
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerpage)
 
-    posts.push(...nextPosts);
-    this.setState({ posts, page: nextPage });
+    posts.push(...nextPosts)
+
+    setPosts(posts);
+    setPage(nextPage);
   };
 
-  handleChange = (event) => {
+  const handleChange = (event) => {
     const { value } = event.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
   };
 
-  async componentDidMount() {
-    await this.handleFetchPosts();
-  }
+  useEffect(() => {
+    handleFetchPosts();
+  }, [handleFetchPosts]);
 
-  render() {
-    const { posts, page, postsPerpage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerpage >= allPosts.length;
 
-    const filteredPosts = !!searchValue
-      ? allPosts.filter((post) => {
-          return post.title.toLowerCase().includes(searchValue.toLowerCase());
-        })
-      : posts;
+  return (
+    <>
+      <Head>
+        <title>Fetch Posts</title>
+      </Head>
+      <div className={style.homeContainer}>
+        <div className={style.searchContainer}>
+          {!!searchValue && <h1>Search: {searchValue}</h1>}
 
-    return (
-      <>
-        <Head>
-          <title>Fetch Posts</title>
-        </Head>
-        <div className={style.homeContainer}>
-          <div className={style.searchContainer}>
-            {!!searchValue && <h1>Search: {searchValue}</h1>}
-
-            <Input
-              placeholder="Search Posts"
-              onChange={this.handleChange}
-              value={searchValue}
-            />
-          </div>
-
-          {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-          {filteredPosts.length === 0 && <h2>Post não encontrado</h2>}
-
-          <div className={style.buttonContainer}>
-            {!searchValue &&
-              (noMorePosts ? (
-                <Button disabled>Load more Posts</Button>
-              ) : (
-                <Button onClick={this.loadMorePosts}>Load more Posts</Button>
-              ))}
-          </div>
+          <Input
+            placeholder="Search Posts"
+            onChange={handleChange}
+            value={searchValue}
+          />
         </div>
-      </>
-    );
-  }
-}
+
+        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+        {filteredPosts.length === 0 && <h2>Post não encontrado</h2>}
+
+        <div className={style.buttonContainer}>
+          {!searchValue &&
+            (noMorePosts ? (
+              <Button disabled>Load more Posts</Button>
+            ) : (
+              <Button onClick={loadMorePosts}>Load more Posts</Button>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
